cf0e6bcc082cb3ac2a030828a8c4838774fbbcf9
sush-tech <sushmithar31@gmail.com>
2024-02-12 17:20:59 -0500

    added ratings at the end of payment

--- Client/src/pages/Success.jsx
+++ Client/src/pages/Success.jsx
@@ -2,9 +2,34 @@ import { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+import StarIcon from '@mui/icons-material/Star';
+
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
+
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
+
+  const [value, setValue] = React.useState(2);
+  const [hover, setHover] = React.useState(-1);
 
   useEffect(() => {
     async function saveOrder() {
@@ -22,7 +47,7 @@ function Success() {
 
       setTimeout(() => {
         window.location.assign('/');
-      }, 3000);
+      }, 10000);
     }
 
     saveOrder();
@@ -33,8 +58,33 @@ function Success() {
       <div
       style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
     >
-     <h1>Success!</h1>
+    
+       
+    
+      <h1>Success!</h1>
         <h2>Thank you for your purchase!</h2>
+        <br/>                                     
+        <h2>Please rate your experience</h2>
+        <br/>
+      <Rating 
+        defaultValue={2} 
+        size="large"
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        getLabelText={getLabelText}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      />
+      {value !== null && (
+        <Box>{labels[hover !== -1 ? hover : value]}</Box>
+      )}
+    
         <h2>You will now be redirected to the home page</h2>
         
     </div>
@@ -42,3 +92,17 @@ function Success() {
 }
 
 export default Success;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
