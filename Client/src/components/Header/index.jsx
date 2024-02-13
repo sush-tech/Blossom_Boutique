e305be86ec69c5aa0ce7af272e946daff277bd87
sush-tech <sushmithar31@gmail.com>
2024-02-13 17:39:08 -0500

    header img fixed

--- Client/src/components/Header/index.jsx
+++ Client/src/components/Header/index.jsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+
 
 export default function FixedContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container>
+    
      <Box
     
   component="img"
   className= "header_image"
-  alt="The house from the offer."
-  src="/images/valentine.jpg"
+  alt="rose image"
+  src="/images/hd_rose.jpg"
 />
-      </Container>
+     
     </React.Fragment>
   );
 }
