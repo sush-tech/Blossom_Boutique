49ad7dddddd1bd6755b8c5dc67278d1c695d2176
sush-tech <sushmithar31@gmail.com>
2024-02-13 18:25:46 -0500

    new about us page

---
+++ Client/src/components/SignInSide/index.jsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import InfoIcon from '@mui/icons-material/Info';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Blossom Boutique
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(/images/half.avif)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <InfoIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              About Us
+            </Typography>
+            <Box   sx={{ mt: 1 }}>
+              
+        
+                
+       <p> Mobile number: 214-867-3456</p>
+       <p> email: Sush@gmail.com</p> 
+       <Link href="#" fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}>   Learn More </Link>
+              {/* <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+              
+              </Button> */}
+              {/* <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid> */}
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
--- Client/src/pages/AboutUs.jsx
+++ Client/src/pages/AboutUs.jsx
@@ -1,11 +1,10 @@
+import SignInSide from "../components/SignInSide";
 const AboutUs = () => {
-    console.log("home page");
     return ( 
       <div>
-       <h1>About Us:</h1> 
-       <p> Mobile number: 214-867-3456</p>
-       <p> email: Sush@gmail.com</p>
+       <SignInSide/>
       </div>
+      
     );
   };
   
